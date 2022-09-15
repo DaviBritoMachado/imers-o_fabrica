6bb2ff2172afa04365fe592a71f5a35fec3e19f3
Davi Brito <davbritmachad@gmail.com>
2022-09-15 19:45:37 -0300

    Teste

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def chiclete(request):
+    
+    return render(request, template_name='blog/home.html')
